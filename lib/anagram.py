409d00d4ed873891f23dd3cc722d8fbb3421c66c
dan <dancr.wley@gmail.com>
2023-05-24 18:23:52 -0400

    nad yelwork

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# your code goes he
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        ana_list = []
+        for w in word_list:
+            if sorted(w) == sorted(self.word):
+                ana_list.append(w)
+        return ana_list
